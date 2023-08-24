88789db3b6663a3ee8da3885049967c0878bfaea
Mohamed_Fayyad <mohamedfayad080@gmail.com>
2023-08-24 20:52:01 +0300

    Add info, mkdir, read and remove commands

--- File System Command Line/Program.cs
+++ File System Command Line/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace File_System_Command_Line
+﻿using System.Runtime;
+
+namespace File_System_Command_Line
 {
     internal class Program
     {
@@ -19,7 +21,7 @@
 
                 if (command == "list")
                 {
-                    foreach (var entry in Directory.GetFileSystemEntries(path))
+                    foreach (var entry in Directory.GetDirectories(path))
                     {
                         //[GetFileName] is a {static method} of the [Path] {class}. 
                         //It is used to extract the file or directory name from a given path.
@@ -27,28 +29,55 @@
                         string directoryName = Path.GetFileName(entry);
                         Console.WriteLine($"\t[Dir] {directoryName}");
                     }
-                    //foreach (var entry in Directory.GetFiles(path))
-                    //{
-                    //    string fileName = Path.GetFileName(entry);
-                    //    Console.WriteLine($"\t[File] {Path.GetFileName(fileName)}");
-                    //}
+                    foreach (var entry in Directory.GetFiles(path))
+                    {
+                        string fileName = Path.GetFileName(entry);
+                        Console.WriteLine($"\t[File] {Path.GetFileName(fileName)}");
+                    }
 
                 }
-                else if(command == "info")
+                else if (command == "info")
                 {
+                    if (Directory.Exists(path))
+                    {
+                        var dirInfo = new DirectoryInfo(path);
+                        Console.WriteLine($"Type: Directory");
+                        Console.WriteLine($"Name: {dirInfo.Name}");
+                        Console.WriteLine($"Created At: {dirInfo.CreationTime}");
+                        Console.WriteLine($"Last modified At: {dirInfo.LastWriteTime}");
+                    }
+                    else if (File.Exists(path))
+                    {
+                        var fileInfo = new FileInfo(path);
+                        Console.WriteLine($"Type: {(fileInfo.Extension)[1..]} file");
+                        Console.WriteLine($"Created At: {fileInfo.CreationTime}");
+                        Console.WriteLine($"Last modified At: {fileInfo.LastWriteTime}");
+                        Console.WriteLine($"Size in bytes: {fileInfo.Length}");
 
+                    }
+                    else
+                    {
+                        Console.WriteLine("directory or file not exist");
+                    }
                 }
                 else if (command == "mkdir")
                 {
-
+                    Directory.CreateDirectory(path);
                 }
                 else if (command == "remove")
                 {
-                    
+                    if (Directory.Exists(path))
+                        Directory.Delete(path);
+                    else if (File.Exists(path))
+                        File.Delete(path);
                 }
                 else if (command == "read")
                 {
-
+                    if (File.Exists(path))
+                    {
+                        var content = File.ReadAllText(path);
+                        Console.WriteLine(content);
+                    }
                 }
                 else if (command == "exit")
                 {
@@ -57,7 +86,7 @@
             }
 
 
-        
+
         }
     }
 }
